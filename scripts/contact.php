85ccc43c35eae63d9274fdb1aa85af17d3d222ac
Cyntique <cyntydev@gmail.com>
2022-08-08 17:32:21 -0300

    Cambios en los forms

--- scripts/contact.php
+++ scripts/contact.php
@@ -3,18 +3,18 @@
 // Llamando a los campos
 $nombre = $_POST['name'];
 $correo = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$mensaje = $_POST['message'];
 
 // Datos para el correo
 $destinatario = "info@netcy.ar";
-$asunto = "Contacto desde nuestra web";
+$asunto = "Mensaje de la página de contacto";
 
-$carta = "De: $nombre \n";
+$carta = "De: $nombre \n"; 
 $carta .= "Correo: $correo \n";
 $carta .= "Mensaje: $mensaje";
 
 // Enviando Mensaje
 mail($destinatario, $asunto, $carta);
-header('Location:contacto.html');
+header('Location:envio.html');
 
 ?>
--- scripts/forms.php
+++ scripts/forms.php
@@ -1,6 +1,5 @@
 <?php
-if(isset($_POST['ContactoEnviar']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['patente'])){
-    
+
     // Submitted form data
     $name   = $_POST['name'];
     $apellido  = $_POST['apellido'];
@@ -11,63 +10,30 @@ if(isset($_POST['ContactoEnviar']) && !empty($_POST['name']) && !empty($_POST['e
     $gnc = $_POST['gnc'];
     $comercial = $_POST['comercial'];
     $alarma = $_POST['alarma'];
+
+    $correo = filter_var($correo, FILTER_VALIDATE_EMAIL);
+        if (!$correo) {
+    echo "Correo inválido. Intente con otro correo.";
+    exit;
+    }
     
     /*
      * Send email to admin
      */
-    $to     = 'info@netcy.ar';
-    $subject= 'Solicitud de cotización';
+    $to     = "info@netcy.ar";
+    $asunto= "Solicitud de cotización";
+
+    $datos = "De: $name\nApellido: $apellido\nDNI: $dni\nCodigo Postal: $cp\nEmail: $email\nPatente: $patente\nEquipo GNC: $gnc\n¿Uso Comercial?: $comercial\n¿Alarma?: $alarma\n";
+    $mensaje: "Recibiste una solicitud de cotización aqui los detalles: \n$datos";
     
-    $htmlContent = '<html>
-    <h4>¡Te solicitan una nueva cotización! A continuación los datos ingresados</h4>
-    <table cellspacing="0" style="width: 300px; height: 200px;">
-        <tr>
-            <th>Nombre:</th><td>'.$name.'</td>
-        </tr>
-        <tr style="background-color: #e0e0e0;">
-            <th>Apellido:</th><td>'.$apellido.'</td>
-        </tr>
-        <tr>
-            <th>DNI:</th><td>'.$dni.'</td>
-        </tr>
-        <tr>
-            <th>Código Postal:</th><td>'.$cp.'</td>
-         </tr>
-         <tr>
-            <th>Email:</th><td>'.$email.'</td>
-         </tr>
-         <tr>
-            <th>Código Postal:</th><td>'.$cp.'</td>
-         </tr>
-         <tr>
-            <th>Patente:</th><td>'.$patente.'</td>
-         </tr>
-         <tr>
-            <th>Equipo GNC:</th><td>'.$gnc.'</td>
-        </tr>
-        <tr>
-            <th>Uso comercial:</th><td>'.$comercial.'</td>
-        </tr>
-        <tr>
-            <th>¿Posee Alarma?:</th><td>'.$alarma.'</td>
-        </tr>
-    </table>
-    </html>';
-    
-    // Set content-type header for sending HTML email
-    $headers = "MIME-Version: 1.0" . "rn";
-    $headers .= "Content-type: text/html; charset=\"UTF-8\"\r\nContent-Transfer-Encoding: 8bit\r\n";
-    
-    // Additional headers
-    $headers .= 'From: CodexWorld<sender@example.com>' . "rn";
-    
-    // Send email
-    if(mail($to,$subject,$htmlContent,$headers)){
-        $status = 'ok';
-    }else{
-        $status = 'err';
-    }
-    
-    // Output status
-    echo $status;die;
+   $headers= "Enviado desde: info@netcy.ar";
+   $headers .= "Desde $name  <" . $correo . ">\r\n";
+
+   $resultado = mail($to, $asunto, $mensaje, $headers);
+
+   if ($resultado) {
+    echo "<h1>Mensaje enviado, ¡Gracias por contactarme!</h1>";
+    echo "<p>Tu mensaje se ha enviado correctamente.</p><h2>Importante</h2><p>Revisa tu bandeja de SPAM, en ocasiones mis respuestas quedan ahí. </p>";
+} else {
+    echo "Tu mensaje no se ha enviado. Intenta de nuevo.";
 }
